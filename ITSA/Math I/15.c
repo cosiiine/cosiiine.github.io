538bb3e4b2735e0665728c3b9d0ae4d43a37283c
cosiiine <82078222+cosiiine@users.noreply.github.com>
2022-01-18 13:18:49 +0800

    Create 15.c

---
+++ ITSA/Math I/15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+  
+int main(){  
+    int a,b;  
+  
+    while(scanf("%d %d",&a,&b)!=EOF)  
+    {  
+        if(a>100||b>100||a<0||b<0) printf("outside\n");  
+        else printf("inside\n");  
+    }  
+  
+    return 0;  
+}
